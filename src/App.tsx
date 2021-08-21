c2b59815cda99f1e1f39f4b6acf346b97549850f
Alexander Leon <alexanderleon@Alexanders-MacBook-Pro.local>
2021-08-21 10:31:49 -0400

    add to-do app UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [todos, setTodos] = React.useState<any[]>([]);
+  const [inputText, setInputText] = React.useState("");
+
+  const onAddTodo = () => {
+    if (!inputText) {
+      return alert("Nothing to add...");
+    }
+    setTodos([...todos, {
+      createdAt: new Date().toISOString(),
+      message: inputText
+    }])
+    setInputText("");
+  }
+
+  const onRemoveTodo = (createdAt: string) => {
+    setTodos(todos.filter((todo: any) => todo.createdAt !== createdAt));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To-Do</h1>
+      <input value={inputText} onChange={(e) => setInputText(e.target.value)} placeholder="I need to..." />
+      <button onClick={onAddTodo}>Add!</button>
+      <div className="todos-content">
+        {
+          todos.map((todo) => (
+            <div key={todo.createdAt} className="todo-list-item">
+              <p>{todo.message}</p>
+              <p onClick={() => onRemoveTodo(todo.createdAt)} className="close-icon">X</p>
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
